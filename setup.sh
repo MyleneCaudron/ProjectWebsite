96f9a636836eabaf854161d136556e196559ae15
Celina Bermudez <m.celina.bermudez@gmail.com>
2022-06-07 11:47:21 +0200

    initial test heroku playlistprediction

--- setup.sh
+++ setup.sh
@@ -4,3 +4,10 @@ echo "\
 [general]\n\
 email = \"m.celina.bermudez@gmail.com\"\n\
 " > ~/.streamlit/config.toml
+
+echo "\
+[server]\n\
+hardless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
